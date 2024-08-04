c2377c00f6f5966a216e4882581c214995fbc026
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-04 17:59:35 +0200

    Commented the latest exception example (NumberFormatException)

--- src/main/java/es/fercbrt/srpingbooterrorshandler/controller/AppController.java
+++ src/main/java/es/fercbrt/srpingbooterrorshandler/controller/AppController.java
@@ -1,18 +1,36 @@
 package es.fercbrt.srpingbooterrorshandler.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import es.fercbrt.srpingbooterrorshandler.models.User;
+import es.fercbrt.srpingbooterrorshandler.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/controller")
 public class AppController {
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     String index() {
         // This will throw an ArithmeticException
         //int value = 100 / 0;
         // This will throw a NumberFormatException
-        int value = Integer.parseInt("abc");
+        //int value = Integer.parseInt("abc");
         return "Hello World!";
+    }
+
+    @GetMapping("/users")
+    ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/users/{id}")
+    ResponseEntity<User> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findById(id));
     }
 }
