a1fc597da9e1af6afd23161632376667991e7bb6
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-04 17:26:11 +0200

    Initial commit

---
+++ src/main/java/es/fercbrt/srpingbooterrorshandler/SrpingbootErrorsHandlerApplication.java
@@ -0,0 +1,13 @@
+package es.fercbrt.srpingbooterrorshandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingbootErrorsHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingbootErrorsHandlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/fercbrt/srpingbooterrorshandler/SrpingbootErrorsHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fercbrt.srpingbooterrorshandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingbootErrorsHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
